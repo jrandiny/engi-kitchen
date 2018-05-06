01b19889f0d707e5f4029c32761d0970289d4970
jrandiny <jrandiny2@gmail.com>
2018-05-06 20:45:38 +0700

    Perbaiki tester simulasi

--- Test/TestSimulasi.pas
+++ Test/TestSimulasi.pas
@@ -1,61 +1,102 @@
 Program testSimulasi;
-uses USimulasi;
+
+uses USimulasi,UFile;
+
 var
-	error : boolean;
-	i : integer;
+	IsError : boolean;
+  Input:string;
+  InputAngka:longint;
+  InputAngka2:longint;
+  IsJalan : boolean;
+
 begin
-	error := false;
-	startSimulasi(1);
-	lihatStatistik();
-{	writeln();
-	writeln('TESTING Besok');
-	tidur(error);
-	if not error then begin
-		lihatStatistik();
-	end;
-	writeln();
-	writeln('TESTING Besok');
-	tidur(error);
-	if not error then begin
-		lihatStatistik();
-	end;}
-{	writeln();
-	writeln('TESTING PakeEnergi');
-	for i := 1 to 9 do begin
-		pakeEnergi();
-	end;
-	lihatStatistik();
-	writeln();
-	writeln('TESTING Istirahat 2X');
-	istirahat();
-	lihatStatistik();
-	writeln();
-	istirahat();
-	lihatStatistik();
-	writeln();
-	writeln('TESTING Makan 4X');
-	makan();
-	lihatStatistik();
-	writeln();
-	makan();
-	lihatStatistik();
-	writeln();
-	makan();
-	lihatStatistik();
-	writeln();
-	makan();
-	lihatStatistik();
-	writeln();
-	writeln('TESTING Besok');
-	tidur(error);
-	if not error then begin
-		lihatStatistik();
-	end;}
- 	writeln();
-	writeln('TESTING UbahSTATISTIK');
-	ubahStatistik(1,3);
-	ubahStatistik(2,1);
-	ubahStatistik(3,2);
-	ubahStatistik(4,2);
-	lihatStatistik();
+	writeln('TESTING - USimulasi');
+  writeln();
+
+  writeln('LOAD');
+  SemuaSimulasi := getSimulasi(parse('simulasi.txt'));
+  IsJalan:=false;
+
+  while(true)do
+  begin
+    writeln();
+    writeln('TESTING - USimulasi');
+    write('Perintah : ');
+    readln(input);
+
+    if(IsJalan)then
+    begin
+      case input of
+        'makan':begin
+          makan();
+        end;
+        'istirahat':begin
+          istirahat()
+        end;
+        'tidur':begin
+          tidur(IsError);
+        end;
+        'tambahUang':begin
+          write('Jumlah penambahan : ');
+          readln(InputAngka);
+          tambahUang(InputAngka);
+        end;
+        'pakaiUang':begin
+          write('Jumlah pengurangan : ');
+          readln(InputAngka);
+          pakaiUang(InputAngka,IsError);
+        end;
+        'lihat':begin
+          lihatStatistik();
+        end;
+        'ubah':begin
+          writeln('1 = jumlah mentah dibeli');
+          writeln('2 = jumlah bahan diolah');
+          writeln('3 = jumlah olahan dijual');
+          writeln('4 = jumlah resep dijual}');
+          write('Kode statistik : ');
+          readln(InputAngka);
+          write('Jumlah perubahan : ');
+          readln(InputAngka2);
+          ubahStatistik(InputAngka,InputAngka2);
+        end;
+      end;
+
+    end else
+    begin
+      case input of
+        'start':begin
+          write('Nomor simulasi : ');
+          readln(InputAngka);
+          startSimulasi(InputAngka,IsError);
+          if(not(IsError))then
+          begin
+            IsJalan:=true;
+          end else
+          begin
+            IsJalan:=false;
+          end;
+        end;
+      end;
+    end;
+
+    case input of
+      'help':begin
+        writeln('exit       -> Keluar');
+        writeln('istirahat  -> Istirahat');
+        writeln('lihat      -> Lihat statistik');
+        writeln('makan      -> Makan');
+        writeln('pakaiUang  -> Memakai uang');
+        writeln('start      -> Memulai simulasi');
+        writeln('tambahUang -> Menambahkan uang');
+        writeln('tidur      -> Memulai hari baru');
+        writeln('ubah       -> Ubah statistik');
+      end;
+      'exit':begin
+        break;
+      end;
+    end;
+
+
+  end;
 end.
