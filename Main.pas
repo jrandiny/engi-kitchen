3958e4bc930479ed10e27ea162e0e1ea55a5808b
jrandiny <jrandiny2@gmail.com>
2018-05-15 11:48:17 +0700

    Update Main menggunakan UUI (2/2)

--- Main.pas
+++ Main.pas
@@ -36,67 +36,6 @@ const
     sortArray();
   end;
 
-  procedure commandPredictor(input:string;Daftar:DaftarPerintah);
-  {I.S. : Terdefinisi perintah-perintah yang mungkin}
-  {F.S. : Dicetak ke layar command yang paling mirip}
-
-  {KAMUS LOKAL}
-  const
-    MINCOCOK=0.49;
-
-  var
-    Kecocokan : real;
-    i,j:integer;
-    Total:integer;
-    Sama:integer;
-    MaxCocok:real;
-    IndeksCocok:array[1..NMAX]of integer;
-    Neff:integer;
-
-  begin
-
-    {search}
-    MaxCocok:=MINCOCOK;
-    Neff:=1;
-    for i:=1 to Daftar.Neff do
-    begin
-      sama:=0;
-      total:=Length(input);
-      for j:=1 to total do
-      begin
-        {Jika panjang input lebih dari perintah, dianggap ketidaksesuain}
-        if(j<=Length(Daftar.Isi[i]))then
-        begin
-          {Jika huruf sama, tambah kesesuaian}
-          if(Daftar.Isi[i][j]=input[j])then
-          begin
-            sama:=sama+1;
-          end;
-        end;
-      end;
-
-      {Cari persentase kesesuaian}
-      Kecocokan:=sama/total;
-      if(Kecocokan>MaxCocok)then
-      begin
-        MaxCocok:=Kecocokan;
-        IndeksCocok[Neff]:=i;
-      end;
-    end;
-
-    {Output yang cocok}
-    if((MaxCocok-MINCOCOK)>0.0001)then
-    begin
-      writelnText('Mungkin yang dimaksud :');
-      for i:=1 to Neff do
-      begin
-        writelnText(Daftar.Isi[IndeksCocok[i]]);
-      end;
-    end;
-
-
-  end;
-
   procedure hentikanSimulasi();
   {I.S. Simulasi berjalan}
   {F.S. Simulasi berhenti}
@@ -108,104 +47,7 @@ const
     SemuaSimulasi.Isi[SimulasiAktif.Nomor]:=SimulasiAktif;
 	end;
 
-  function bacaInput(Input:string):UserInput;
-  {memecah Input ke perintah, opsi1, dan opsi2}
 
-  {Kamus lokal}
-  var
-    i: integer;
-    Indeks: integer;
-    Sementara: string;
-    Hasil:UserInput;
-    Separator:char;
-
-  {Algoritma}
-  begin
-    Sementara:= '';
-    i:=1;
-    Indeks:=1;
-    Hasil.Perintah := '';
-    Hasil.Neff:=0;
-    Separator:=' ';
-
-    {Looping semua input}
-    while (i<=length(Input)) do
-    begin
-
-      {Jika separator, data biasa, atau habis}
-      if ((Input[i]='"')and(Separator=' '))then
-      begin
-        Separator:='"';
-      end else if ((Input[i]=Separator) or (i=length(Input))) then
-      begin
-
-        {Jika habis langsung tambahkan input terakhir}
-        if(i=length(Input))then
-        begin
-          {tambahkan kecuali jika "}
-          if (Separator<>'"')then
-          begin
-            Sementara := Sementara + Input[i];
-          end;
-        end;
-
-        {Masukkan ke tempat yang benar}
-        if (Indeks=1)then
-        begin
-          Hasil.Perintah:=Sementara;
-        end else
-        begin
-          Hasil.Neff:=Hasil.Neff+1;
-          Hasil.Opsi[Hasil.Neff]:=Sementara;
-        end;
-
-        Indeks:=Indeks+1;
-        Sementara:='';
-
-        {Skip satu jika separator menggunakan "}
-        if (Separator='"')then
-        begin
-          Separator:=' ';
-          i:=i+1;
-        end;
-
-      end else
-      begin
-        Sementara:=Sementara + Input[i];
-      end;
-
-      i:=i+1;
-
-    end;
-
-    bacaInput:=Hasil;
-  end;
-
-  procedure formatUserInput(var Diketik:string);
-  {I.S. : terdefinisi input Diketik}
-  {F.S. : Mengubah menjadi format ProperCase}
-
-  var
-    n: integer;
-  begin
-    if (length(Diketik) > 0) then
-    begin
-      Diketik[1] := UpCase(Diketik[1]);
-      n := 2;
-      while (n <= length(Diketik)) do
-      begin
-        Diketik[n] := LowerCase(Diketik[n]);
-        if (Diketik[n] = ' ') then
-        begin
-          Diketik[n+1] := UpCase(Diketik[n+1]);
-          n := n + 2;
-        end else
-        begin
-          n := n + 1;
-        end;
-      end;
-    end;
-  end;
 
 procedure loadData();
 {I.S. : variabel di unit-unit kosong}
@@ -282,8 +124,7 @@ begin
           begin
             if (InputTerproses.Neff = 0) then
             begin
-        			write('Nomor simulasi: ');
-        			readln(InputTerproses.Opsi[1]);
+              tanya('Nomor simulasi',InputTerproses.Opsi[1]);
             end;
             Val(InputTerproses.Opsi[1],OpsiAngka,KodeError);
             if(KodeError<>0)then
@@ -325,15 +166,13 @@ begin
         'belibahan' : begin
     		  if (InputTerproses.Neff < 1 ) then
     		  begin
-      			write('Nama bahan: ');
-      			readln(InputTerproses.Opsi[1]);
+      			tanya('Nama bahan',InputTerproses.Opsi[1]);
     		  end;
           formatUserInput(InputTerproses.Opsi[1]);
 
           if (InputTerproses.Neff < 2) then
           begin
-            write('Jumlah bahan: ');
-      			readln(InputTerproses.Opsi[2]);
+            tanya('Jumlah bahan',InputTerproses.Opsi[2]);
           end;
           Val(InputTerproses.Opsi[2],OpsiAngka,KodeError);
 
@@ -358,8 +197,7 @@ begin
         'olahbahan' : begin
           if (InputTerproses.Neff = 0) then
           begin
-            write('Nama bahan olahan: ');
-            readln(InputTerproses.Opsi[1]);
+            tanya('Nama bahan olahan',InputTerproses.Opsi[1]);
           end;
           formatUserInput(InputTerproses.Opsi[1]);
           pakeEnergi(Error);
@@ -376,14 +214,12 @@ begin
         'jualolahan' : begin
           if (InputTerproses.Neff < 1) then
           begin
-            write('Nama bahan olahan: ');
-    			  readln(InputTerproses.Opsi[1]);
+            tanya('Nama bahan olahan',InputTerproses.Opsi[1])
           end;
           formatUserInput(InputTerproses.Opsi[1]);
 		      if (InputTerproses.Neff < 2) then
     		  begin
-    			  write('Jumlah bahan olahan untuk dijual: ');
-    			  readln(InputTerproses.Opsi[2]);
+    			  tanya('Jumlah bahan olahan untuk dijual',InputTerproses.Opsi[2]);
     		  end;
     		  Val(InputTerproses.Opsi[2],OpsiAngka,KodeError);
 
@@ -408,8 +244,7 @@ begin
         'jualresep' : begin
           if (InputTerproses.Neff = 0) then
           begin
-      			write('Nama resep: ');
-      			readln(InputTerproses.Opsi[1]);
+            tanya('Nama resep',InputTerproses.Opsi[1])
     		  end;
           formatUserInput(InputTerproses.Opsi[1]);
 
@@ -485,8 +320,7 @@ begin
         begin
           if (InputTerproses.Neff = 0) then
           begin
-            write('Nomor Inventori: ');
-            readln(InputTerproses.Opsi[1]);
+            tanya('Nomor inventori',InputTerproses.Opsi[1]);
           end;
           Val(InputTerproses.Opsi[1],OpsiAngka,KodeError);
     		  if(KodeError<>0)then
@@ -515,8 +349,7 @@ begin
       'cariresep' : begin
         if (InputTerproses.Neff = 0) then
         begin
-          write('Nama resep: ');
-          readln(InputTerproses.Opsi[1]);
+          tanya('Nama resep',InputTerproses.Opsi[1]);
         end;
         formatUserInput(InputTerproses.Opsi[1]);
         cariResep(InputTerproses.Opsi[1]);
@@ -525,14 +358,12 @@ begin
       'tambahresep' : begin
         if(InputTerproses.Neff = 0) then
         begin
-      		write('Nama resep: ');
-      		readln(InputTerproses.Opsi[1]);
+      	  tanya('Nama resep',InputTerproses.Opsi[1]);
     	  end;
         formatUserInput(InputTerproses.Opsi[1]);
 
         ResepBaru.Nama := InputTerproses.Opsi[1];
-        write('Harga: ');
-        readln(StringInput);
+        tanya('Harga',StringInput);
         Val(StringInput,HargaResep,KodeError);
         if(KodeError<>0) then
         begin
@@ -540,8 +371,7 @@ begin
   		  end else
   		  begin
   		    ResepBaru.Harga := HargaResep;
-    			write('Jumlah bahan: ');
-    			readln(StringInput);
+    			tanya('Jumlah bahan',StringInput);
     			Val(StringInput,OpsiAngka,KodeError);
     			if(KodeError<>0) then
     			begin
@@ -552,8 +382,7 @@ begin
     			  writelnText('Daftar bahan: ');
             for i := 1 to ResepBaru.JumlahBahan do
             begin
-              write('   ', i, '. ');
-              readln(ResepBaru.Bahan[i]);
+              tanya('   Bahan '+ IntToStr(i),ResepBaru.Bahan[i]);
               formatUserInput(ResepBaru.Bahan[i]);
     			  end;
 			      tambahResep(ResepBaru, Error);
